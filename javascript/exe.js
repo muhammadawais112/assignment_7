a91d8b5110ab93fc09512960a07f0a7aec7fb414
muhammadawais112 <simpleboy1917@gmail.com>
2021-12-08 17:53:49 +0500

    practise

---
+++ javascript/exe.js
@@ -0,0 +1,208 @@
+// PROGRAM:1
+var  a= prompt("ENTER A ");
+ var b =prompt("ENTER B");
+ if(a>b){
+     console.log("A is a maximum number",a);
+ }
+ else{
+     console.log("B is a maximum number",b);
+ }
+  document.write(a>b)
+// program : 2
+// var a =prompt("ENTER A NUM1");
+
+// var b =prompt("ENTER A NUM2");
+//  var c= prompt("ENTER A NUM3");
+//  if(a>b &&b<a){
+//      console.log("A is maximum number",a);
+
+//  }
+//  if(c>b&&b<c){
+//      console.log("c is maximum number",c);
+//  }
+//  else{
+//     console.log("B is maximum number",b);
+// }
+// var n = prompt("ENTER A NUMBER ");
+// if(n==0)
+// {
+//     console.log("number is zero",n);
+// }
+// else{
+// if (n > 0) {
+//     console.log("NUMBER IS A POSITIVE", n);
+// }
+// else{
+//     console.log("NUMBER IS A NEGATIVE", n);
+// }
+// }
+// program :4
+// var n =prompt("ENTER A NUMBER");
+// if(n%5==0 && n%11==0){
+//     console.log("NUMBER IS DIVIDE BY 5 AND 11");
+// }
+// else{
+//     console.log("NUMBER IS NOT DIVIDED BY 5 AND 11");
+// }
+// program 5
+// var n=prompt("ENTER A NUMBER")
+// if(n%2==0){
+//     console.log("ENTER A EVEN NUMBER");
+// }
+// else{
+//     console.log("ENTER A ODD NUMBER");
+// }
+// program 6
+// var y =prompt("enter a year");
+// if(y%4==0){
+//     console.log("ENTER A LEEP YEAR ");
+// }
+// else{
+// console.log("ENTER NOT A LEAP YEAR");
+// }
+// program :8
+// var ch=prompt('ENTER A CHARACTER')
+// if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='u'||ch=='U'){
+//     console.log("ENTER A VOWEL ",ch);
+
+// }
+// else{
+//     console.log("ENTER NOT A VOWEL NUMBER",ch);
+// }
+// program11
+// var n = prompt("enter a number");
+// if (n == 1) {
+//     console.log("MONDAY");
+// }
+// if(n==2){
+//     console.log("TUESDAY");
+// }
+// if(n==3){
+//     console.log("WENESDAY");
+// }
+// if(n==4){
+//     console.log("THURSDAY");
+// }
+// if(n==5){
+//     console.log("FRIDAY");
+// }
+// if(n==6){
+//     console.log("SATURSDAY");
+// }
+// if(n==7){
+//     console.log("SUNDAY");
+// }
+// else{
+//     console.log("ENTER A INVALID WEEK DAY NUMBER",n);
+// }    
+// var n = prompt("ENTER A MONTH NUMBER")
+// if (n == 1) {
+//     console.log("JANUARY/n 31");
+// }
+// if (n == 2) {
+//     console.log("FEBRUARY  28");
+// }
+// if (n == 3) {
+//     console.log("MARCH 31");
+// }
+// if (n == 4) {
+//     console.log("APRIL 30");
+// }
+// if (n == 5) {
+//     console.log("MAY 31");
+// }
+// if (n == 6) {
+//     console.log("JUNE 30");
+// }
+// if (n == 7) {
+//     console.log("JULY 31");
+// }
+// if (n == 8) {
+//     console.log("AUGUST 31");
+// }
+// if (n == 9) {
+//     console.log("SEPTEMBER 31");
+// }
+// if (n == 10) {
+//     console.log("OCTOBER 31");
+// }
+// if (n == 11) {
+//     console.log("NOVEMBER 30");
+// } if (n == 12) {
+//     console.log("DECEMBER 31");
+// }
+// else {
+//     console.log("enter a invalid number");
+
+// // }
+// Program:12
+// var n = prompt("ENTER A MONTH NUMBER")
+// if (n == 1) {
+//     console.log("JANUARY/n 31");
+// }
+// else if (n == 2) {
+//     console.log("FEBRUARY  28");
+// }
+// else if (n == 3) {
+//     console.log("MARCH 31");
+// }
+// else if (n == 4) {
+//     console.log("APRIL 30");
+// }
+// else if (n == 5) {
+//     console.log("MAY 31");
+// }
+// else if (n == 6) {
+//     console.log("JUNE 30");
+// }
+// else if (n == 7) {
+//     console.log("JULY 31");
+// }
+// else if (n == 8) {
+//     console.log("AUGUST 31");
+// }
+// else if (n == 9) {
+//     console.log("SEPTEMBER 31");
+// }
+// else if (n == 10) {
+//     console.log("OCTOBER 31");
+// }
+// else if (n == 11) {
+//     console.log("NOVEMBER 30");
+// } else if (n == 12) {
+//     console.log("DECEMBER 31");
+// }
+// else {
+//     console.log("enter a invalid number");
+
+// }
+// program :14
+// var m1 = prompt("ENTER A PHYSICS MARKS");
+// var m2 = prompt("ENTER A  CHEMISTRY MARKS");
+// var m3 = prompt("ENTER A  BIOLOGY MARKS");
+// var m4 = prompt("ENTER A MATH MARKS");
+// var m5 = prompt("ENTER A COMPUTER MARKS");
+// t_marks = 500;
+// var per = (m1 + m2 + m3 + m4 + m5 / t_marks) * 100
+// if (per < 40) {
+//     console.log("GRADE F");
+// }
+// else if (per >= 40 && per < 60) {
+//     console.log("Grade E ",per);
+// }
+// else if (per >= 60 && per < 70) {
+//     console.log("Grade D ",per);
+// }
+// else if (per >= 70 && per < 80) {
+//     console.log("Grade c ",per);
+// }
+// else if (per >= 80 && per < 90) {
+//     console.log("Grade B",per);
+// }
+// else if (per >= 90 && per < 100) {
+//     console.log("Grade A ",per);
+// }
+// else {
+//     console.log("your grade is A++");
+// }
+
